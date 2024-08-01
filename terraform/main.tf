ed116514c5da83a21d5e16f475569f5f4f587843
beast86m <47268167+beast86m@users.noreply.github.com>
2024-08-02 03:35:07 +0500

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,13 @@
-
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+  required_version = ">= 0.47.0"
+}
+provider "yandex" {
+  token     = "y0_AgAAAAA7Zu****************************************K45LgQ"
+  cloud_id  = "b1g9s62rvesc1uus58qv"
+  folder_id = "b1g2chgudk1u4u9g4j61"
+}
